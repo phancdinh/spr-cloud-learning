b17bc32180f4a62547e458618afb372775218c54
dinh.phan <congdinhphan@gmail.com>
2021-05-19 17:10:56 +0700

    add service discovery

--- kuber/kuberclient/src/main/java/com/example/kuberclient/controller/StudentController.java
+++ kuber/kuberclient/src/main/java/com/example/kuberclient/controller/StudentController.java
@@ -3,17 +3,24 @@ package com.example.kuberclient.controller;
 import com.example.kuberclient.clientServer.StudentClient;
 import com.example.kuberclient.dto.StudentDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
     private final StudentClient studentClient;
+    private final DiscoveryClient discoveryClient;
     private RestTemplate rest = new RestTemplateBuilder().build();
 
-    public StudentController(StudentClient studentClient) {
+    public StudentController(StudentClient studentClient, DiscoveryClient discoveryClient) {
         this.studentClient = studentClient;
+        this.discoveryClient = discoveryClient;
     }
 
     @GetMapping(value = "/")
@@ -32,5 +39,9 @@ public class StudentController {
     @GetMapping(value = "/student2")
     public String student22() {
         return rest.getForObject("http://k8s-demo-app/student", String.class);
+    }
+    @GetMapping(value = "/services/{name}")
+    public List<ServiceInstance> service(@PathVariable String name) {
+        return discoveryClient.getInstances(name);
     }
 }
